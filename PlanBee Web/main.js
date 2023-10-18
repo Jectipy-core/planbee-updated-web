be4ac15b71163d99e840c4e02cdf5259709e57c5
Yakuphan Bilmez <yakuphan95@gmail.com>
2023-10-18 08:25:56 +0300

    PlanBee Updated
    changeyourmind

---
+++ PlanBee Web/main.js
@@ -0,0 +1,49 @@
+// DOSYA_YOLU: c:\Users\yakup\OneDrive\Masaüstü\PlanBee Web\main.js
+// BAŞLANGIÇ: ed8c6549bwf9
+// kaydırma işlemi sırasında gezinme çubuğu stillerini değiştirin
+// SON: ed8c6549bwf9
+
+window.addEventListener('scroll', () => { 
+    const nav = document.querySelector('nav');
+    document.querySelector('nav').classList.toggle ('window-scroll', window.scrollY > 100);
+});
+
+
+// show/hide FAQ answer
+const faqs = document.querySelectorAll('.faq'); 
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus' ){
+            icon.className = "uil uil-minus";
+        } else {
+            icon.className = "uil uil-plus";
+        }
+    });
+});
+
+
+// show/hide mobile menu
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    closeBtn.style.display = 'inline-block';
+    menuBtn.style.display = 'none';
+});
+
+
+// click nav menu 
+const closeNav = () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+}
+
+closeBtn.addEventListener('click', closeNav);
